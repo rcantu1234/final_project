0f6e96dc7327353fe0d4cbca929e929e6502c610
ROEL CANTU <roel.cantu@yahoo.com>
2015-03-10 12:04:05 -0500

    Testing Maps

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,9 +4,6 @@ class Location < ActiveRecord::Base
 
   validates :zip_code, presence: true
 
-  def self.get_coordinates()
-  end
-
   geocoded_by :zip_code
 
   after_validation :geocode, :if => :zip_code_changed?
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   #devise_for :users
 
+  #match '/request' => 'location#create', via: :get
 
   root 'welcome#index'
 
