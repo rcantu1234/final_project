66c00c0fb87fab5eb3c33e775997f445510d3b3f
ROEL CANTU <roel.cantu@yahoo.com>
2015-03-13 12:54:15 -0500

    Testing features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name, :address,
                                    :city, :state, :zip_code)
