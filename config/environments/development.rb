0dcb0ae986b8408b00681ab4226100f260a3000b
ROEL CANTU <roel.cantu@yahoo.com>
2015-03-18 22:54:27 -0500

    Testing Website

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,6 +46,9 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
